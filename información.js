aff49341111e0d1b0f643520ae8b5e5bd15cc4f9
Gardo <nixito_16@hotmail.com>
2025-04-03 23:09:41 -0300

    pagina de practica

---
+++ información.js
@@ -0,0 +1,15 @@
+/**
+*TODO: HTML
+
+**Metadatos
+son datos sobre la pagina web que no se muestra en el contenido visible,
+
+*TODO: Otros
+
+**XML
+es un lenguaje de marcado que se utiliza para almacenar y transportar datos de
+forma estructurada. Es un estándar desarrollado por el W3C y es utilizado en
+muchas aplicaciones para el intercambio de datos. antes de JSON, XML era el
+estándar para el intercambio de datos en la web, a diferencia de JSON, XML
+esta basado en etiquetas <tag>.
+ */
